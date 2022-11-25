3038b27f9fbb7efc6f28fc1950b8cf469d35cf78
pelumi <polalekan@gmail.com>
2022-11-25 15:12:46 +0100

    filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from datetime import datetime
+from models import storage
 import uuid
 
 
@@ -8,13 +9,19 @@ class BaseModel():
     BaseModel class defining all common attributes/methods
     for other classes that would be used on this project
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize instance of BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is None or len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            self.id = kwargs['id']
+            self.created_at = datetime.fromisoformat(kwargs['created_at'])
+            self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
 
     def __str__(self):
         """
@@ -27,6 +34,7 @@ class BaseModel():
         Records the last time instance of Basemodel was updated
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+import json
+import os
+
+
+class FileStorage():
+    """
+    Storage engine class that serializes instances
+    to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """
+        returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file
+        """
+        obj_dicts = {**self.__objects}
+        for key, obj in obj_dicts.items():
+            obj_dicts[key] = obj.to_dict()
+
+        buf = json.dumps(obj_dicts)
+
+        with open(self.__file_path, 'w') as fd:
+            fd.write(buf)
+
+    def classes(self):
+        """
+        Returns a dictionary of valid classes and their references
+        """
+        from models.base_model import BaseModel
+        # from models.user import User
+        # from models.state import State
+        # from models.city import City
+        # from models.amenity import Amenity
+        # from models.place import Place
+        # from models.review import Review
+
+        classes = {
+                    "BaseModel": BaseModel,
+                    # "User": User,
+                    # "State": State,
+                    # "City": City,
+                    # "Amenity": Amenity,
+                    # "Place": Place,
+                    # "Review": Review
+                    }
+        return classes
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        only if the JSON file (__file_path) exists
+        """
+        if not os.path.exists(self.__file_path):
+            return
+
+        with open(self.__file_path, 'r') as fd:
+            json_dicts = json.loads(fd.read())
+
+        for key, value in json_dicts.items():
+            json_dicts[key] = self.classes()[value['__class__']](**value)
+
+        self.__objects = {**json_dicts}
+
+
